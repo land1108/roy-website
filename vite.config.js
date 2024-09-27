0f995f3775da923f7f5fb746d87d9bd5459b27d7
Roy Zou <94884829+land1108@users.noreply.github.com>
2024-09-27 11:43:43 +0800

    first deploy

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: '/roy-website/', // 替换为您的仓库名称
+  build: {
+    outDir: 'dist'
+  }
+})
